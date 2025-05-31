3fd9e26f081a6da767ee4bd809ca5fe585419d08
JShostak <23fmif.yu.shostak@std.udu.edu.ua>
2025-05-31 08:26:17 +0300

    Merge branch 'feature/multiply'

--- calculator.py
+++ calculator.py
@@ -1,12 +1,20 @@
 # calculator.py
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
 def multiply(x, y):
     return x * y
+
 if __name__ == "__main__":
     num1 = 15
     num2 = 7
+
     print(f"{num1} + {num2} = {add(num1, num2)}")
     print(f"{num1} - {num2} = {subtract(num1, num2)}")
-# Додаємо виклик нової функції
-    print(f"{num1} * {num2} = {multiply(num1, num2)}")
-# Змінюємо повідомлення
-    print("Калькулятор оновлено: додано множення.") # Змінений рядок!
+    print(f"{num1} * {num2} = {multiply(num1, num2)}")  # З feature/multiply
+
+    print("Виправлено текст повідомлення у main, інтегровано множення.")  # Комбіноване повідомлення
